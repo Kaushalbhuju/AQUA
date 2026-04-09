e78b2be2bc3e1869ce458c50674cf72fd4ceaccb
Aqua <idsnew467@gmail.com>
2026-04-09 21:54:22 +0545

    domain fixed at the localhost and remove api

--- rm_system/urls.py
+++ rm_system/urls.py
@@ -289,7 +289,6 @@ urlpatterns = [
     path('coe-visa/', include('coe_visa.urls')),
     path('other-documents/', include('other_documents.urls')),
     path('books/', include('books.urls', namespace='books')),
-    path('api/', include('api.urls')),
 
 ]
 
